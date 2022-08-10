fa6bcc75b44237783ce6c891ff1ab6b0a0017555
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-08-10 18:40:21 +1000

    start.sh go brrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- start.sh
+++ start.sh
@@ -1,48 +1,58 @@
-#!/bin/sh
-AIKAR_FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
-JAVA_FLAGS="-Xms${MEMORY} -Xmx${MEMORY} ${AIKAR_FLAGS}"
+#!/bin/bash
+AIKAR_FLAGS=(-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true)
+JAVA_FLAGS=(-Xms${MEMORY} -Xmx${MEMORY})
 
+# Create plugins folder (if non-existent)
 cd /data
-mkdir plugins -p
+if [ ! -f plugins ]
+then
+    mkdir -p plugins
+else
+	rm -r plugins
+	mkdir plugins
+fi
 
-jenkins_dl() {
-    api_server=$1
-    job=$2
-    build_type=$3
-    filename_test=$4
-    relative_download_url=$(curl -s ${api_server}/job/${job}/${build_type}/api/json | jq -r --arg filename_test ${filename_test} '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
-    echo ${api_server}/job/${job}/${build_type}/artifact/${relative_download_url}
+jenkins() {
+	api=$1
+	job=$2
+	build_type=$3
+	filename_test=$4
+	out_path=$5
+    relative_url=$(curl -s ${api}/job/${job}/${build_type}/api/json | jq -r --arg filename_test ${filename_test} '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
+    echo ${api}/job/${job}/${build_type}/artifact/${relative_url}
 }
 
-# Download urls
-coreprotect() {
-    curl -s https://api.github.com/repos/PlayPro/CoreProtect/releases/latest | jq -r '.assets[0].browser_download_url'
-}
-discordsrv() {
-    echo https://download.discordsrv.com/snapshot
-}
-paperclip() {
-    api_server=https://api.papermc.io
-    latest_version_build=$(curl -s ${api_server}/v2/projects/paper/versions/${MINECRAFT_VERSION}/builds | jq '.builds[-1]')
-    build_id=$(echo ${latest_version_build} | jq '.build')
-    build_filename=$(echo ${latest_version_build} | jq -r '.downloads.application.name')
-    echo ${api_server}/v2/projects/paper/versions/${MINECRAFT_VERSION}/builds/${build_id}/downloads/${build_filename}
-}
-spark() {
-    jenkins_dl https://ci.lucko.me spark lastSuccessfulBuild bukkit
-}
-squaremap() {
-    jenkins_dl https://jenkins.jpenilla.xyz squaremap lastSuccessfulBuild paper
+# Plugin Download URLs
+CoreProtect=$(curl -s https://api.github.com/repos/PlayPro/CoreProtect/releases/latest | jq -r '.assets[0].browser_download_url')
+DiscordSRV=https://download.discordsrv.com/snapshot
+Spark=$(jenkins https://ci.lucko.me spark lastSuccessfulBuild bukkit)
+SquareMap=$(jenkins https://jenkins.jpenilla.xyz squaremap lastSuccessfulBuild paper)
+PaperMC() {
+	api=https://api.papermc.io
+	main_path=v2/projects/paper/versions/${MINECRAFT_VERSION}/builds
+	build_metadata=$(curl -s ${api}/${main_path} | jq '.builds[-1]')
+	build_id=$(echo ${build_metadata} | jq '.build')
+	build_name=$(echo ${build_metadata} | jq -r '.downloads.application.name')
+	echo ${api}/${main_path}/${build_id}/downloads/${build_name}
 }
 
 # Download jars
-echo Downloading jars
-for url in coreprotect discordsrv paperclip spark squaremap
+for url in $(PaperMC) ${CoreProtect} ${DiscordSRV} ${Spark} ${SquareMap}
 do
-    curl -sL $($url) -o $url.jar && echo Dowloaded $url &
-done
-wait
+	echo "Downloading $(basename ${url%.*})..."
+	if [[ ! $url =~ "api.papermc.io" ]]
+	then
+		curl -sL ${url} -o plugins/$(basename ${url%.*}).jar &
+	else
+		curl -sL ${url} -o paperclip.jar &
+	fi
+done & wait
 
-# Run server
-echo "eula=true" > eula.txt
-java ${JAVA_FLAGS} -jar paperclip.jar --world-dir worlds
+# Accept EULA if it hasn't been
+if [ ! -f ./eula.txt ]
+then
+    echo "eula=true" > eula.txt
+fi
+
+# Start server
+java ${AIKAR_FLAGS} ${JAVA_FLAGS} --world-dir worlds -jar paperclip.jar
