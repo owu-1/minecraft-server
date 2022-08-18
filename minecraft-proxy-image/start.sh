887de0bb667aceac756f75b34b6785454d05e75c
owu <83254308+owu-1@users.noreply.github.com>
2022-08-18 17:27:36 +1000

    Hardcode into AWS image

--- minecraft-proxy-image/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-RECOMMENDED_STARTUP_FLAGS="-XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15"
-java -Xms$MEMORY -Xmx$MEMORY $RECOMMENDED_STARTUP_FLAGS -jar /data/velocity.jar
