0853c5282a53eda59bfdde022598aac193931f9d
owu <83254308+owu-1@users.noreply.github.com>
2022-09-07 02:49:28 +1000

    update

--- mods.sh
+++ mods.sh
@@ -1,5 +1,5 @@
 curl -L -o plugins/discordsrv.jar https://github.com/DiscordSRV/DiscordSRV/releases/download/v1.25.1/DiscordSRV-Build-1.25.1.jar
 curl -L -o plugins/squaremap.jar https://github.com/jpenilla/squaremap/releases/download/v1.1.8/squaremap-paper-mc1.19.2-1.1.8.jar
 curl -L -o plugins/coreprotect.jar https://github.com/PlayPro/CoreProtect/releases/download/v21.2/CoreProtect-21.2.jar
-curl -L -o plugins/multiverse-core.jar http://ci.onarandombox.com/job/Multiverse-Core/861/artifact/target/Multiverse-Core-4.3.1.jar
-curl -L -o plugins/multiverse-inventories.jar http://ci.onarandombox.com/job/Multiverse-Inventories/523/artifact/target/Multiverse-Inventories-4.2.3.jar
+curl -L -o plugins/myworlds.jar https://ci.mg-dev.eu/job/MyWorlds/168/artifact/target/MyWorlds-1.19.2-v1-SNAPSHOT-168.jar
+curl -L -o plugins/bkcommonlib.jar https://ci.mg-dev.eu/job/BKCommonLib/1352/artifact/target/BKCommonLib-1.19.2-v1-1352.jar
