6a019f5a4149c95f49d2030a20ed494545c0c864
owu <83254308+owu-1@users.noreply.github.com>
2022-09-07 19:06:49 +1000

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p plugins
 mkdir -p data/lists
-mkdir -p data/plugin/{coreprotect,my_worlds,squaremap}
-touch data/coreprotect/database.db
+mkdir -p data/plugin/{coreprotect,my_worlds}
 touch data/lists/{banned-ips.json,banned-players.json,whitelist.json}
-touch data/my_worlds/{inventories.yml,worlds.yml}
+touch data/plugin/coreprotect/database.db
+touch data/plugin/my_worlds/{inventories.yml,worlds.yml}
 curl -L -o plugins/luckperms.jar https://download.luckperms.net/1453/bukkit/loader/LuckPerms-Bukkit-5.4.46.jar
 curl -L -o plugins/discordsrv.jar https://github.com/DiscordSRV/DiscordSRV/releases/download/v1.25.1/DiscordSRV-Build-1.25.1.jar
 curl -L -o plugins/squaremap.jar https://github.com/jpenilla/squaremap/releases/download/v1.1.8/squaremap-paper-mc1.19.2-1.1.8.jar
