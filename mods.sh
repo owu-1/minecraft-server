039a0a00370d52d3b5df14941d66e54c0e8cfee7
owu <83254308+owu-1@users.noreply.github.com>
2022-09-07 17:43:49 +1000

    update

--- mods.sh
+++ mods.sh
@@ -1,3 +1,5 @@
+mkdir -p plugins
+curl -L -o plugins/luckperms.jar https://download.luckperms.net/1453/bukkit/loader/LuckPerms-Bukkit-5.4.46.jar
 curl -L -o plugins/discordsrv.jar https://github.com/DiscordSRV/DiscordSRV/releases/download/v1.25.1/DiscordSRV-Build-1.25.1.jar
 curl -L -o plugins/squaremap.jar https://github.com/jpenilla/squaremap/releases/download/v1.1.8/squaremap-paper-mc1.19.2-1.1.8.jar
 curl -L -o plugins/coreprotect.jar https://github.com/PlayPro/CoreProtect/releases/download/v21.2/CoreProtect-21.2.jar
