5491528baeefaeee6fd843085d4eea43944c9e6f
owu <83254308+owu-1@users.noreply.github.com>
2022-09-08 01:22:33 +1000

    update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ mkdir -p data/lists
 mkdir -p data/plugin/{coreprotect,my-worlds,luckperms}
 touch data/lists/{banned-ips.json,banned-players.json,whitelist.json}
 touch data/plugin/coreprotect/database.db
-touch data/plugin/my-worlds/inventories.yml
+touch data/plugin/my-worlds/{inventories.yml,worlds.yml}
 touch data/plugin/luckperms/luckperms-h2.mv.db
-cp setup-files/worlds.yml data/plugin/my-worlds/worlds.yml
 ./mods.sh
