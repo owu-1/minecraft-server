eedd0b2f725fd09458bc41f13a734e7e58a849d3
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 13:07:33 +1000

    Fix lint errors

--- mods.sh
+++ mods.sh
@@ -23,8 +23,8 @@ jenkins() {
     local build=$3
     local filename_test=$4
     relative_path=$(curl -s "https://$jenkins_server/job/$job/$build/api/json" | jq -r --arg filename_test "$filename_test" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
-    echo Downloading ${job}...
-    curl -sL "$jenkins_server/job/$job/$build/artifact/$relative_path" -o ${job}-${build}.jar
+    echo "Downloading ${job}..."
+    curl -sL "$jenkins_server/job/$job/$build/artifact/$relative_path" -o $job-$build.jar
 }
 
 github() {
@@ -32,16 +32,16 @@ github() {
     local repo=$2
     local tag=$3
     local filename_test=$4
-    echo Downloading ${repo}...
-    curl -sL "${curl -s "https://api.github.com/repos/$owner/$repo/releases/tags/$tag" | jq -r --arg filename_test "$filename_test" '.assets | .[] | select(.name|test($filename_test)).browser_download_url'}" -o ${repo}-${tag}.jar
+    echo "Downloading ${repo}..."
+    curl -sL "${curl -s "https://api.github.com/repos/$owner/$repo/releases/tags/$tag" | jq -r --arg filename_test "$filename_test" '.assets | .[] | select(.name|test($filename_test)).browser_download_url'}" -o $repo-$tag.jar
 }
 
 modrinth() {
     local project_id=$1
     local version=$2
     local filename_test=$3
-    echo Downloading ${project_id}...
-    curl -sL "${curl -sG "https://api.modrinth.com/v2/project/$project_id/version" --data-urlencode 'loaders=["bukkit"]' | jq -r --arg filename_test "$filename_test" '.[] | select(.version_number|test($filename_test)).files[0].url'}" -o ${project_id}-${version}.jar
+    echo "Downloading ${project_id}..."
+    curl -sL "${curl -sG "https://api.modrinth.com/v2/project/$project_id/version" --data-urlencode 'loaders=["bukkit"]' | jq -r --arg filename_test "$filename_test" '.[] | select(.version_number|test($filename_test)).files[0].url'}" -o $project_id-$version.jar
 }
 
 jenkins ${bergerhealer_ci} MyWorlds ${myworlds_build} '^MyWorlds-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
