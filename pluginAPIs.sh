53a54352d615d243d3091f501d6cc7b7e5a05b41
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 14:01:12 +1000

    added curly brackets back
    fuck lint

--- pluginAPIs.sh
+++ pluginAPIs.sh
@@ -5,9 +5,9 @@ jenkins() {
     local job=$2
     local build=$3
     local filename_test=$4
-    relative_path=$(curl -s "https://$jenkins_server/job/$job/$build/api/json" | jq -r --arg filename_test "$filename_test" ".artifacts | .[] | select(.fileName|test($filename_test)).relativePath")
+    relative_path=$(curl -s "https://$jenkins_server/job/$job/$build/api/json" | jq -r --arg filename_test ${filename_test} '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
     echo "Downloading ${job}..."
-    curl -sL "$jenkins_server/job/$job/$build/artifact/$relative_path" -o "$job-$build.jar"
+    curl -sL "$jenkins_server/job/$job/$build/artifact/$relative_path" -o ${job}-${build}.jar
 }
 
 github() {
@@ -16,7 +16,7 @@ github() {
     local tag=$3
     local filename_test=$4
     echo "Downloading ${repo}..."
-    curl -sL "$(curl -s "https://api.github.com/repos/$owner/$repo/releases/tags/$tag" | jq -r --arg filename_test "$filename_test" ".assets | .[] | select(.name|test($filename_test)).browser_download_url")" -o "$repo-$tag.jar"
+    curl -sL "$(curl -s "https://api.github.com/repos/$owner/$repo/releases/tags/$tag" | jq -r --arg filename_test $filename_test '.assets | .[] | select(.name|test($filename_test)).browser_download_url')" -o ${repo}-${tag}.jar
 }
 
 modrinth() {
@@ -24,5 +24,5 @@ modrinth() {
     local version=$2
     local filename_test=$3
     echo "Downloading ${project_id}..."
-    curl -sL "${curl -sG "https://api.modrinth.com/v2/project/$project_id/version" --data-urlencode "loaders=[\"bukkit\"]" | jq -r --arg filename_test "$filename_test" ".[] | select(.version_number|test($filename_test)).files[0].url"}" -o "$project_id-$version.jar"
+    curl -sL "${curl -sG "https://api.modrinth.com/v2/project/$project_id/version" --data-urlencode 'loaders=["bukkit"]' | jq -r --arg filename_test $filename_test '.[] | select(.version_number|test($filename_test)).files[0].url'}" -o ${project_id}-${version}.jar
 }
