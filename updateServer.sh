efd4928c1c823b31e80958173a9094bd40467659
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 15:56:57 +1000

    Use sudo on chmod

--- updateServer.sh
+++ updateServer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo git pull || exit
-chmod +x mods.sh
-chmod +x updateServer.sh
-chmod +x restart.sh
-chmod +x variables.sh
-chmod +x pluginAPIs.sh
+sudo chmod +x mods.sh
+sudo chmod +x updateServer.sh
+sudo chmod +x restart.sh
+sudo chmod +x variables.sh
+sudo chmod +x pluginAPIs.sh
 ./mods.sh
 sudo ./restart.sh
