c3cd9df4755d54d9f9c44e35ad47161b14cad9ff
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 13:56:30 +1000

    fuck lint

--- variables.sh
+++ variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export bergerhealer_ci=ci.mg-dev.eu
-export lucko_ci=ci.lucko.me
+bergerhealer_ci=ci.mg-dev.eu
+lucko_ci=ci.lucko.me
 
-export myworlds_build=172                    # Build must be >=159 to support rejoin world groups with /world lastposition merge [world1] [world2] ...
-export bkcommonlib_build=1390                # Build must be >=1386 to avoid error messages - https://github.com/bergerhealer/BKCommonLib/issues/147
-export coreprotect_version=v21.2
-export discordsrv_version=v1.26.0
-export spark_build=339
-export luckperms_build=1453
-export squaremap_version=v1.1.8
-export simple_voice_chat_version=1.19.2-2.3.6
+myworlds_build=172                    # Build must be >=159 to support rejoin world groups with /world lastposition merge [world1] [world2] ...
+bkcommonlib_build=1390                # Build must be >=1386 to avoid error messages - https://github.com/bergerhealer/BKCommonLib/issues/147
+coreprotect_version=v21.2
+discordsrv_version=v1.26.0
+spark_build=339
+luckperms_build=1453
+squaremap_version=v1.1.8
+simple_voice_chat_version=1.19.2-2.3.6
