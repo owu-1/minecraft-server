0b7454b2c642a1800813240b3e6ee9e70fe0a17a
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 13:42:01 +1000

    Added quotation marks to make lint happy

--- mods.sh
+++ mods.sh
@@ -6,11 +6,11 @@ rm ./*.jar
 source ../variables.sh
 source ../pluginAPIs.sh             # todo: Unnecessary filename tests
 
-jenkins ${bergerhealer_ci} MyWorlds ${myworlds_build} '^MyWorlds-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
-jenkins ${bergerhealer_ci} BKCommonLib ${bkcommonlib_build} '^BKCommonLib-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
-github PlayPro CoreProtect ${coreprotect_version} '^CoreProtect-\d+\.\d+\.jar$'
-github DiscordSRV DiscordSRV ${discordsrv_version} '^DiscordSRV-Build-\d+\.\d+\.\d+\.jar$'
-jenkins ${lucko_ci} spark ${spark_build} '^spark-\d+.\d+.\d+-bukkit\.jar$'
-jenkins ${lucko_ci} LuckPerms ${luckperms_build} '^LuckPerms-Bukkit-\d+.\d+.\d+\.jar$'
-github jpenilla squaremap ${squaremap_version} '^squaremap-paper-mc.+-\d+\.\d+\.\d+\.jar$'
-modrinth simple-voice-chat ${simple_voice_chat_version} ^bukkit-${simple_voice_chat_version}$
+jenkins "${bergerhealer_ci}" MyWorlds "${myworlds_build}" '^MyWorlds-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
+jenkins "${bergerhealer_ci}" BKCommonLib "${bkcommonlib_build}" '^BKCommonLib-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
+github PlayPro CoreProtect "${coreprotect_version}" '^CoreProtect-\d+\.\d+\.jar$'
+github DiscordSRV DiscordSRV "${discordsrv_version}" '^DiscordSRV-Build-\d+\.\d+\.\d+\.jar$'
+jenkins "${lucko_ci}" spark "${spark_build}" '^spark-\d+.\d+.\d+-bukkit\.jar$'
+jenkins "${lucko_ci}" LuckPerms "${luckperms_build}" '^LuckPerms-Bukkit-\d+.\d+.\d+\.jar$'
+github jpenilla squaremap "${squaremap_version}" '^squaremap-paper-mc.+-\d+\.\d+\.\d+\.jar$'
+modrinth simple-voice-chat "${simple_voice_chat_version}" '^bukkit-${simple_voice_chat_version}$'
