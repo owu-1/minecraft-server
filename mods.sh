a4dcf894135824601f465d67c494996388e99f9c
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 14:42:49 +1000

    Fix typo

--- mods.sh
+++ mods.sh
@@ -8,9 +8,9 @@ source ../pluginAPIs.sh             # todo: Unnecessary filename tests
 
 jenkins ${bergerhealer_ci} MyWorlds ${myworlds_build} '^MyWorlds-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
 jenkins ${bergerhealer_ci} BKCommonLib ${bkcommonlib_build} '^BKCommonLib-.+-v\d+-(?:SNAPSHOT-)?\d+\.jar'
-gitrelease PlayPro CoreProtect ${coreprotect_version} '^CoreProtect-\d+\.\d+\.jar$'
-gitrelease DiscordSRV DiscordSRV ${discordsrv_version} '^DiscordSRV-Build-\d+\.\d+\.\d+\.jar$'
+ghrelease PlayPro CoreProtect ${coreprotect_version} '^CoreProtect-\d+\.\d+\.jar$'
+ghrelease DiscordSRV DiscordSRV ${discordsrv_version} '^DiscordSRV-Build-\d+\.\d+\.\d+\.jar$'
 jenkins ${lucko_ci} spark ${spark_build} '^spark-\d+.\d+.\d+-bukkit\.jar$'
 jenkins ${lucko_ci} LuckPerms ${luckperms_build} '^LuckPerms-Bukkit-\d+.\d+.\d+\.jar$'
-gitrelease jpenilla squaremap ${squaremap_version} '^squaremap-paper-mc.+-\d+\.\d+\.\d+\.jar$'
+ghrelease jpenilla squaremap ${squaremap_version} '^squaremap-paper-mc.+-\d+\.\d+\.\d+\.jar$'
 modrinth simple-voice-chat ${simple_voice_chat_version} '^bukkit-${simple_voice_chat_version}$'
