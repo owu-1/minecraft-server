72714a4cc43c7066d51a2c7f1e5cebf879bf0958
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-10-02 15:03:23 +1000

    Fixed my fuck ups

--- pluginAPIs.sh
+++ pluginAPIs.sh
@@ -2,24 +2,24 @@
 
 # GitHub Releases
 # Usage: github user repo version filenameTest
-ghrelease() { echo "Downloading $2...";curl -sLo $2-$3.jar $(curl -s "https://api.github.com/repos/$1/$2/releases/tags/$3" | jq -r --arg filename_test $4 '.assets | .[] | select(.name|test($4)).browser_download_url');}
+ghrelease() { local user=$1;local repo=$2;local version=$3;local filename_test=$4;echo "Downloading $repo...";curl -sLo $repo-$version.jar $(curl -s "https://api.github.com/repos/$user/$repo/releases/tags/$version" | jq -r --arg filename_test $filename_test '.assets | .[] | select(.name|test($filename_test)).browser_download_url');}
 
 # Modrinth
-# Usage: modrinth project filenameTest
-modrinth() { echo "Downloading $1...";curl -sLo $1-$2.jar $(curl -sG "https://api.modrinth.com/v2/project/$1/version" --data-urlencode 'loaders=["bukkit"]&game_versions=["$VERSION"]' | jq -r --arg filename_test $3 '.[] | select(.version_number|test($3)).files[0].url');}
+# Usage: modrinth project version filenameTest
+modrinth() { local project=$1;local filename_test=$2;echo "Downloading $project...";curl -sLo $project-$version.jar $(curl -sG "https://api.modrinth.com/v2/project/$project/version" --data-urlencode 'loaders=["$LOADER"]&game_versions=["$VERSION"]' | jq -r --arg filename_test $filename_test '.[] | select(.version_number|test($filename_test)).files[0].url');}
 
 # Jenkins
 # Usage: jenkins api project type filenameTest
-jenkins() { echo "Downloading $2...";curl -sLo $2-$3.jar "$1/job/$2/$3/artifact/$(curl -s "https://$1/job/$2/$3/api/json" | jq -r --arg filename_test $4 '.artifacts | .[] | select(.fileName|test($4)).relativePath')";}
+jenkins() { local api=$1;local job=$2;local build=$3;local filename_test=$4;echo "Downloading $job...";curl -sLo $job-$build.jar $api/job/$job/$build/artifact/${curl -s "$api/job/$job/$build/api/json" | jq -r --arg filename_test $filename_test '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath'};}
 
 # Latest GitHub Releases
 # Usage: latest_gitrel user repo fileIndex
-latest_ghrelease() { echo "Downloading $2...";echo --url $(curl -s "https://api.github.com/repos/$1/$2/releases" | jq -r '.[0].assets['$3'].browser_download_url') | curl -sLo $2.jar --config -;}
+latest_ghrelease() { local user=$1;local repo=$2;local index=$3;echo "Downloading $repo...";echo --url $(curl -s "https://api.github.com/repos/$user/$repo/releases" | jq -r '.[0].assets['$index'].browser_download_url') | curl -sLo $repo.jar --config -;}
 
 # Latest Modrinth
 # Usage: latest_modrinth project
-latest_modrinth() { echo "Downloading $1...";curl -sLo $1.jar $(curl -s "https://api.modrinth.com/v2/project/$1/version" --data-urlencode 'loaders=["bukkit"]&game_versions=["$VERSION"]' | jq -r '.[0]'.files[0].url);}
+latest_modrinth() { local project=$1;echo "Downloading $project...";curl -sLo $project.jar $(curl -s "https://api.modrinth.com/v2/project/$project/version" --data-urlencode 'loaders=["$LOADER"]&game_versions=["$VERSION"]' | jq -r '.[0]'.files[0].url);}
 
 # Latest Jenkins
 # Usage: latest_jenkins api project filenameTest
-latest_jenkins(){ echo "Downloading $2...";local file=$(curl -s "$1/job/$2/lastSuccessfulBuild/api/json" | jq -r --arg filename_test $3 '.artifacts | .[] | select(.fileName|test($3)).relativePath' | cut -d ' ' -f1);curl -sLo $file "$1/job/$2/lastSuccessfulBuild/artifact/$file";}
+latest_jenkins(){ local api=$1;local job=$2;local build=$3;local filename_test=$4;echo "Downloading $job...";local file=$(curl -s "$api/job/$job/lastSuccessfulBuild/api/json" | jq -r --arg filename_test $filename_test '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath' | cut -d ' ' -f1);curl -sLo $file "$1/job/$2/lastSuccessfulBuild/artifact/$file";}
