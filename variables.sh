bb13ea2e9a299d03805e60699170651c53776f5a
owu <83254308+owu-1@users.noreply.github.com>
2022-10-04 09:21:18 +1000

    Update plugins

--- variables.sh
+++ variables.sh
@@ -6,11 +6,11 @@ LOADER=bukkit
 bergerhealer_ci=ci.mg-dev.eu
 lucko_ci=ci.lucko.me
 
-myworlds_build=172                    # Build must be >=159 to support rejoin world groups with /world lastposition merge [world1] [world2] ...
-bkcommonlib_build=1390                # Build must be >=1386 to avoid error messages - https://github.com/bergerhealer/BKCommonLib/issues/147
+myworlds_build=175                    # Build must be >=159 to support rejoin world groups with /world lastposition merge [world1] [world2] ...
+bkcommonlib_build=1393                # Build must be >=1386 to avoid error messages - https://github.com/bergerhealer/BKCommonLib/issues/147
 coreprotect_version=v21.2
 discordsrv_version=v1.26.0
-spark_build=339
-luckperms_build=1453
+spark_build=340
+luckperms_build=1456
 squaremap_version=v1.1.8
-simple_voice_chat_version=1.19.2-2.3.6
+simple_voice_chat_version=1.19.2-2.3.10
