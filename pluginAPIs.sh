21cdac3e64fb44e10a2ebc65c1f6f5b5383f89cb
owu <83254308+owu-1@users.noreply.github.com>
2022-10-04 10:05:08 +1000

    Create download function

--- pluginAPIs.sh
+++ pluginAPIs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Usage: download url filename
+download() {
+    echo "Downloading $2"
+    curl -L "$1" -o "$2" --progress-bar 
+}
+
 # GitHub Releases
 # Usage: github user repo version filenameTest
 ghrelease() {
@@ -7,9 +13,8 @@ ghrelease() {
     local repo=$2
     local version=$3
     local filename_test=$4
-    echo "Downloading $repo..."
     url=$(curl -s "https://api.github.com/repos/$user/$repo/releases/tags/$version" | jq -r --arg filename_test "$filename_test" '.assets | .[] | select(.name|test($filename_test)).browser_download_url')
-    curl --progress-bar -Lo "$repo-$version.jar" "$url"
+    download "$url" "$repo-$version.jar"
 }
 
 # Modrinth
@@ -17,9 +22,8 @@ ghrelease() {
 modrinth() {
     local project=$1
     local filename_test=$2
-    echo "Downloading $project..."
     url=$(curl -s "https://api.modrinth.com/v2/project/$project/version?loaders=[%22$LOADER%22]&game_versions=[%22$VERSION%22]" | jq -r --arg filename_test "$filename_test" '.[] | select(.version_number|test($filename_test)).files[0].url')
-    curl --progress-bar -Lo "$project-$filename_test.jar" "$url"
+    download "$url" "$project-$filename_test.jar"
 }
 
 # Jenkins
@@ -30,8 +34,7 @@ jenkins() {
     local build=$3
     local filename_test=$4
     relative_path=$(curl -s "https://$api/job/$job/$build/api/json" | jq -r --arg filename_test "$filename_test" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
-    echo "Downloading $job..."
-    curl --progress-bar -Lo "$job-$build.jar" "https://$api/job/$job/$build/artifact/$relative_path"
+    download "https://$api/job/$job/$build/artifact/$relative_path" "$job-$build.jar" 
 }
 
 # Latest GitHub Releases
@@ -40,18 +43,16 @@ latest_ghrelease() {
     local user=$1
     local repo=$2
     local index=$3
-    echo "Downloading $repo..."
     url=$(curl -s "https://api.github.com/repos/$user/$repo/releases" | jq -r '.[0].assets['"$index"'].browser_download_url')
-    curl --progress-bar -Lo "$repo.jar" --config - "$url"
+    download "$url" "$repo.jar"
 }
 
 # Latest Modrinth
 # Usage: latest_modrinth project
 latest_modrinth() {
     local project=$1
-    echo "Downloading $project..."
     url=$(curl -s "https://api.modrinth.com/v2/project/$project/version" --data-urlencode 'loaders=["$LOADER"]&game_versions=["$VERSION"]' | jq -r '.[0]'.files[0].url)
-    curl --progress-bar -Lo "$project.jar" "$url"
+    download "$url" "$project.jar"
 }
 
 # Latest Jenkins
@@ -61,7 +62,6 @@ latest_jenkins() {
     local job=$2
     local build=$3
     local filename_test=$4
-    echo "Downloading $job..."
     relative_path=$(curl -s "$api/job/$job/lastSuccessfulBuild/api/json" | jq -r --arg filename_test "$filename_test" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath' | cut -d ' ' -f1)
-    curl --progress-bar -Lo "$job" "$1/job/$2/lastSuccessfulBuild/artifact/$relative_path"
+    download "$1/job/$2/lastSuccessfulBuild/artifact/$relative_path" "$job.jar"
 }
