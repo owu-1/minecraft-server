724a1c63526d0a0ce17b675f2fe05d51e69d9f32
owu <83254308+owu-1@users.noreply.github.com>
2022-10-04 10:36:30 +1000

    Replace named params and document

--- pluginAPIs.sh
+++ pluginAPIs.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-# Usage: download url filename
+# Download a file
+# $1 - url
+# $2 - filename
 download() {
     echo "Downloading $2"
     curl -L "$1" -o "$2" --progress-bar 
 }
 
-# GitHub Releases
-# Usage: github user repo version filenameTest
+# Download an asset from a github release
+# https://docs.github.com/en/rest/releases/releases#get-a-release-by-tag-name
+# $1 - owner: The account owner of the repository
+# $2 - repo: The name of the repository
+# $3 - tag: The release tag
+# $4 - pattern: A regex pattern matching the filename of the asset
 ghrelease() {
-    local user=$1
-    local repo=$2
-    local version=$3
-    local filename_test=$4
-    url=$(curl -s "https://api.github.com/repos/$user/$repo/releases/tags/$version" | jq -r --arg filename_test "$filename_test" '.assets | .[] | select(.name|test($filename_test)).browser_download_url')
-    download "$url" "$repo-$version.jar"
+    url=$(curl -s "https://api.github.com/repos/$1/$2/releases/tags/$3" | jq -r --arg filename_test "$4" '.assets | .[] | select(.name|test($filename_test)).browser_download_url')
+    download "$url" "$2-$3.jar"
 }
 
-# Modrinth
-# Usage: modrinth project filenameTest
+# Download a file from a Modrinth project version
+# https://docs.modrinth.com/api-spec/#tag/versions/operation/getProjectVersions
+# $1 - id: The ID or slug of the project
+# $2 - version
+# $2 - pattern: A regex pattern matching the version number
 modrinth() {
-    local project=$1
-    local filename_test=$2
-    url=$(curl -s "https://api.modrinth.com/v2/project/$project/version?loaders=[%22$LOADER%22]&game_versions=[%22$VERSION%22]" | jq -r --arg filename_test "$filename_test" '.[] | select(.version_number|test($filename_test)).files[0].url')
-    download "$url" "$project-$filename_test.jar"
+    url=$(curl -s "https://api.modrinth.com/v2/project/$1/version?loaders=[%22$LOADER%22]&game_versions=[%22$VERSION%22]" | jq -r --arg filename_test "$3" '.[] | select(.version_number|test($filename_test)).files[0].url')
+    download "$url" "$1-$2.jar"
 }
 
-# Jenkins
-# Usage: jenkins api project type filenameTest
+# Download an artifact from a Jenkins build
+# $1 - jenkins_server: The Jenkin server's domain name
+# $2 - job: The project name
+# $3 - build: The build number or description
+# $4 - pattern: A regex pattern matching the filename
 jenkins() {
-    local api=$1
-    local job=$2
-    local build=$3
-    local filename_test=$4
-    relative_path=$(curl -s "https://$api/job/$job/$build/api/json" | jq -r --arg filename_test "$filename_test" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
-    download "https://$api/job/$job/$build/artifact/$relative_path" "$job-$build.jar" 
+    build_url="https://$1/job/$2/$3"
+    relative_path=$(curl -s "$build_url/api/json" | jq -r --arg filename_test "$4" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
+    download "$build_url/artifact/$relative_path" "$2-$3.jar" 
 }
