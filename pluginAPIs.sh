7a05c66c915d46c1613ab3bfb734f131a1bc0216
owu <83254308+owu-1@users.noreply.github.com>
2022-10-04 10:07:10 +1000

    Remove download latest functions
    Repeating code. Better to have functions which return the latest version, then pass that version to the existing download functions

--- pluginAPIs.sh
+++ pluginAPIs.sh
@@ -36,32 +36,3 @@ jenkins() {
     relative_path=$(curl -s "https://$api/job/$job/$build/api/json" | jq -r --arg filename_test "$filename_test" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath')
     download "https://$api/job/$job/$build/artifact/$relative_path" "$job-$build.jar" 
 }
-
-# Latest GitHub Releases
-# Usage: latest_gitrel user repo fileIndex
-latest_ghrelease() {
-    local user=$1
-    local repo=$2
-    local index=$3
-    url=$(curl -s "https://api.github.com/repos/$user/$repo/releases" | jq -r '.[0].assets['"$index"'].browser_download_url')
-    download "$url" "$repo.jar"
-}
-
-# Latest Modrinth
-# Usage: latest_modrinth project
-latest_modrinth() {
-    local project=$1
-    url=$(curl -s "https://api.modrinth.com/v2/project/$project/version" --data-urlencode 'loaders=["$LOADER"]&game_versions=["$VERSION"]' | jq -r '.[0]'.files[0].url)
-    download "$url" "$project.jar"
-}
-
-# Latest Jenkins
-# Usage: latest_jenkins api project filenameTest
-latest_jenkins() {
-    local api=$1
-    local job=$2
-    local build=$3
-    local filename_test=$4
-    relative_path=$(curl -s "$api/job/$job/lastSuccessfulBuild/api/json" | jq -r --arg filename_test "$filename_test" '.artifacts | .[] | select(.fileName|test($filename_test)).relativePath' | cut -d ' ' -f1)
-    download "$1/job/$2/lastSuccessfulBuild/artifact/$relative_path" "$job.jar"
-}
